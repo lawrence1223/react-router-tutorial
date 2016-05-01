dad6e4aa321374ba2f9a5bcdd29c1dab7f020106
Lawrence Luo <lawrenceluo198812@gmail.com>
2016-05-01 14:43:31 -0500

    Implement sample app based on tutorial

--- lessons/01-setting-up/index.js
+++ lessons/01-setting-up/index.js
@@ -1,4 +1,15 @@
 import React from 'react'
+import {Router, Route, browserHistory, IndexRoute} from 'react-router'
 import { render } from 'react-dom'
 import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+import About from './modules/About'
+import Repos from './modules/Repos'
+import Repo from './modules/Repo'
+import Home from './modules/Home'
+import routes from './modules/routes'
+
+
+render(<Router routes={routes} history={browserHistory}>
+
+
+</Router>, document.getElementById('app'))
